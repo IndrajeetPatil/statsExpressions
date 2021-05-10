44c28527d060884bbe91fd2662c151502a50a443
Indrajeet Patil <inderonline1988@gmail.com>
2021-05-10 10:06:40 +0200

    change lintr and simplify tests

--- tests/testthat/test-meta_random_parametric.R
+++ tests/testthat/test-meta_random_parametric.R
@@ -1,32 +1,32 @@
-if (require("metafor")) {
-  test_that(
-    desc = "meta_analysis works - parametric",
-    code = {
-      options(tibble.width = Inf)
-      skip_if(getRversion() < "4.0")
 
-      # dataframe
-      df_eg <-
-        structure(
-          list(
-            estimate = c(0.111, 0.245, 0.8, 1.1, 0.03),
-            std.error = c(0.05, 0.111, 0.001, 0.2, 0.01)
-          ),
-          row.names = c(NA, -5L),
-          class = c("tbl_df", "tbl", "data.frame")
-        )
+test_that(
+  desc = "meta_analysis works - parametric",
+  code = {
+    options(tibble.width = Inf)
+    skip_if(getRversion() < "4.0")
+    skip_if_not_installed("metafor")
 
-      # dataframe output
-      set.seed(123)
-      df <- meta_analysis(df_eg)
+    # dataframe
+    df_eg <-
+      structure(
+        list(
+          estimate = c(0.111, 0.245, 0.8, 1.1, 0.03),
+          std.error = c(0.05, 0.111, 0.001, 0.2, 0.01)
+        ),
+        row.names = c(NA, -5L),
+        class = c("tbl_df", "tbl", "data.frame")
+      )
 
-      # testing all details
-      set.seed(123)
-      expect_snapshot(dplyr::select(df, -expression))
-      expect_snapshot(df$expression[[1]])
+    # dataframe output
+    set.seed(123)
+    df <- meta_analysis(df_eg)
 
-      # error
-      expect_error(meta_analysis(mtcars))
-    }
-  )
-}
+    # testing all details
+    set.seed(123)
+    expect_snapshot(dplyr::select(df, -expression))
+    expect_snapshot(df$expression[[1]])
+
+    # error
+    expect_error(meta_analysis(mtcars))
+  }
+)
--- tests/testthat/test-z_meta_random_bayes.R
+++ tests/testthat/test-z_meta_random_bayes.R
@@ -2,65 +2,64 @@
 # Bayesian meta-analysis often crashes R session on macOS
 # this makes sure this test will be run after all others
 
-if (require("metaBMA")) {
-  test_that(
-    desc = "meta_analysis works - bayesian",
-    code = {
-      skip_if(getRversion() < "4.0")
+test_that(
+  desc = "meta_analysis works - bayesian",
+  code = {
+    skip_if_not_installed("metaBMA")
+    skip_if(getRversion() < "4.0")
 
-      # setup
-      set.seed(123)
+    # setup
+    set.seed(123)
 
-      # creating a dataframe
-      df1 <-
-        structure(
-          .Data = list(
-            term = c("1", "2", "3", "4", "5"),
-            estimate = c(
-              0.382047603321706,
-              0.780783111514665,
-              0.425607573765058,
-              0.558365541235078,
-              0.956473848429961
-            ),
-            std.error = c(
-              0.0465576338644502,
-              0.0330218199731529,
-              0.0362834986178494,
-              0.0480571500648261,
-              0.062215818388157
-            )
+    # creating a dataframe
+    df1 <-
+      structure(
+        .Data = list(
+          term = c("1", "2", "3", "4", "5"),
+          estimate = c(
+            0.382047603321706,
+            0.780783111514665,
+            0.425607573765058,
+            0.558365541235078,
+            0.956473848429961
           ),
-          row.names = c(NA, -5L),
-          class = c("tbl_df", "tbl", "data.frame")
-        )
+          std.error = c(
+            0.0465576338644502,
+            0.0330218199731529,
+            0.0362834986178494,
+            0.0480571500648261,
+            0.062215818388157
+          )
+        ),
+        row.names = c(NA, -5L),
+        class = c("tbl_df", "tbl", "data.frame")
+      )
 
-      # getting bayes factor in favor of null hypothesis
-      set.seed(123)
-      subtitle1 <-
-        suppressWarnings(meta_analysis(
-          type = "bayes",
-          data = df1,
-          k = 3,
-          iter = 1000,
-          summarize = "integrate"
-        ))
+    # getting bayes factor in favor of null hypothesis
+    set.seed(123)
+    subtitle1 <-
+      suppressWarnings(meta_analysis(
+        type = "bayes",
+        data = df1,
+        k = 3,
+        iter = 1000,
+        summarize = "integrate"
+      ))
 
-      set.seed(123)
-      df <-
-        suppressWarnings(meta_analysis(
-          type = "bayes",
-          data = df1,
-          k = 3,
-          iter = 1000,
-          summarize = "integrate",
-          top.text = "ayyo"
-        ))
+    set.seed(123)
+    df <-
+      suppressWarnings(meta_analysis(
+        type = "bayes",
+        data = df1,
+        k = 3,
+        iter = 1000,
+        summarize = "integrate",
+        top.text = "ayyo"
+      ))
 
-      expect_type(df, "list")
-      expect_identical(class(df), c("tbl_df", "tbl", "data.frame"))
+    expect_type(df, "list")
+    expect_identical(class(df), c("tbl_df", "tbl", "data.frame"))
 
-      expect_snapshot(subtitle1$expression[[1]])
-    }
-  )
-}
+    expect_snapshot(subtitle1$expression[[1]])
+  }
+)
