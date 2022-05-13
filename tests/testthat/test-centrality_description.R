a63e28f6818a9936478e8f823ec3a6df69080365
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-05-13 17:27:58 +0200

    anticipate variable name being "variable" (#158)

--- tests/testthat/test-centrality_description.R
+++ tests/testthat/test-centrality_description.R
@@ -14,7 +14,7 @@ test_that(
         k = list(2L, 3L, 3L, 2L),
         conf.level = list(0.89, 0.99, 0.90, 0.50)
       ),
-      .f = statsExpressions::centrality_description
+      .f = centrality_description
     )
 
     set.seed(123)
@@ -32,11 +32,28 @@ test_that(
         k = list(2L, 3L, 3L, 2L),
         conf.level = list(0.89, 0.99, 0.90, 0.50)
       ),
-      .f = statsExpressions::centrality_description
+      .f = centrality_description
     )
 
     set.seed(123)
     expect_snapshot(select(df_na, -expression))
     expect_snapshot(df_na$expression)
+  }
+)
+
+
+test_that(
+  desc = "centrality description works when variable is named `variable`",
+  code = {
+    options(tibble.width = Inf)
+
+    df_var <- dplyr::rename(mtcars, variable = cyl)
+
+    set.seed(123)
+    res <- suppressWarnings(centrality_description(df_var, variable, wt))
+
+    set.seed(123)
+    expect_snapshot(select(res, -expression))
+    expect_snapshot(res$expression)
   }
 )
