f7efabc0bbbdbadbb5f03fa7c4b9f7b41b430ef7
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-23 21:26:24 +0200

    further trim down on re-exports

--- R/reexports.R
+++ R/reexports.R
@@ -9,23 +9,9 @@ magrittr::`%>%`
 magrittr::`%<>%`
 
 #' @export
-#' @importFrom magrittr "%$%"
-magrittr::`%$%`
-
-#' @export
-#' @importFrom magrittr "%T>%"
-magrittr::`%T>%`
-
-#' @export
 #' @importFrom tibble tibble
 tibble::tibble
 
 #' @export
-#' @importFrom tibble enframe
-tibble::enframe
-
-#' @export
 #' @importFrom tibble as_tibble
 tibble::as_tibble
-
-
