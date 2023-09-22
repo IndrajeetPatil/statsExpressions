0720447dfc15860a1d3eeba39d40f0456e2f4d97
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-09-22 13:02:13 +0200

    fix precommit workflow

--- shell/metadata.sh
+++ shell/metadata.sh
@@ -1,8 +1,10 @@
-# Update metadata for package
+#!/bin/bash
 
+# Update metadata for package
+#
 # To run on
-# - mac:     > sh <filename>.sh
-# - windows: > bash <filename>.sh
+#  - mac:     > sh <filename>.sh
+#  - windows: > bash <filename>.sh
 
 Rscript -e 'roxygen2::roxygenise()'
 Rscript -e 'codemetar::write_codemeta()'
