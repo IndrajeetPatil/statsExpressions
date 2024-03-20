60273aefa98cb54f145ddcacb11a2b170a256583
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-03-20 18:03:53 +0100

    don't run crosstabs examples on CRAN

--- man/examples/examples-contingency-table.R
+++ man/examples/examples-contingency-table.R
@@ -1,77 +1,79 @@
-#### -------------------- association test ------------------------ ####
+if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+  #### -------------------- association test ------------------------ ####
 
-# ------------------------ frequentist ---------------------------------
+  # ------------------------ frequentist ---------------------------------
 
-# unpaired
+  # unpaired
 
-set.seed(123)
-contingency_table(
-  data   = mtcars,
-  x      = am,
-  y      = vs,
-  paired = FALSE
-)
+  set.seed(123)
+  contingency_table(
+    data   = mtcars,
+    x      = am,
+    y      = vs,
+    paired = FALSE
+  )
 
-# paired
+  # paired
 
-paired_data <- tibble(
-  response_before = structure(c(1L, 2L, 1L, 2L), levels = c("no", "yes"), class = "factor"),
-  response_after = structure(c(1L, 1L, 2L, 2L), levels = c("no", "yes"), class = "factor"),
-  Freq = c(65L, 25L, 5L, 5L)
-)
+  paired_data <- tibble(
+    response_before = structure(c(1L, 2L, 1L, 2L), levels = c("no", "yes"), class = "factor"),
+    response_after = structure(c(1L, 1L, 2L, 2L), levels = c("no", "yes"), class = "factor"),
+    Freq = c(65L, 25L, 5L, 5L)
+  )
 
-set.seed(123)
-contingency_table(
-  data   = paired_data,
-  x      = response_before,
-  y      = response_after,
-  paired = TRUE,
-  counts = Freq
-)
+  set.seed(123)
+  contingency_table(
+    data   = paired_data,
+    x      = response_before,
+    y      = response_after,
+    paired = TRUE,
+    counts = Freq
+  )
 
-# ------------------------ Bayesian -------------------------------------
+  # ------------------------ Bayesian -------------------------------------
 
-# unpaired
+  # unpaired
 
-set.seed(123)
-contingency_table(
-  data = mtcars,
-  x = am,
-  y = vs,
-  paired = FALSE,
-  type = "bayes"
-)
+  set.seed(123)
+  contingency_table(
+    data = mtcars,
+    x = am,
+    y = vs,
+    paired = FALSE,
+    type = "bayes"
+  )
 
-# paired
+  # paired
 
-set.seed(123)
-contingency_table(
-  data = paired_data,
-  x = response_before,
-  y = response_after,
-  paired = TRUE,
-  counts = Freq,
-  type = "bayes"
-)
+  set.seed(123)
+  contingency_table(
+    data = paired_data,
+    x = response_before,
+    y = response_after,
+    paired = TRUE,
+    counts = Freq,
+    type = "bayes"
+  )
 
-#### -------------------- goodness-of-fit test -------------------- ####
+  #### -------------------- goodness-of-fit test -------------------- ####
 
-# ------------------------ frequentist ---------------------------------
+  # ------------------------ frequentist ---------------------------------
 
-set.seed(123)
-contingency_table(
-  data   = as.data.frame(HairEyeColor),
-  x      = Eye,
-  counts = Freq
-)
+  set.seed(123)
+  contingency_table(
+    data   = as.data.frame(HairEyeColor),
+    x      = Eye,
+    counts = Freq
+  )
 
-# ------------------------ Bayesian -------------------------------------
+  # ------------------------ Bayesian -------------------------------------
 
-set.seed(123)
-contingency_table(
-  data   = as.data.frame(HairEyeColor),
-  x      = Eye,
-  counts = Freq,
-  ratio  = c(0.2, 0.2, 0.3, 0.3),
-  type   = "bayes"
-)
+  set.seed(123)
+  contingency_table(
+    data   = as.data.frame(HairEyeColor),
+    x      = Eye,
+    counts = Freq,
+    ratio  = c(0.2, 0.2, 0.3, 0.3),
+    type   = "bayes"
+  )
+}
